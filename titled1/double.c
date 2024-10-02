e398ced81f1fceda0b16e8931300d5924d026b08
calculus610 <cals0605@163.com>
2024-10-02 21:29:23 +0800

    1

---
+++ titled1/double.c
@@ -0,0 +1,9 @@
+//
+// Created by ROG on 2024/10/2.
+//
+#include <stdio.h>
+
+int main() {
+    printf("Hello\n");
+    return 0;
+}
---
+++ titled2/single.c
@@ -0,0 +1,9 @@
+//
+// Created by ROG on 2024/10/2.
+//
+#include <stdio.h>
+
+int main() {
+    printf("123\n");
+    return 0;
+}
